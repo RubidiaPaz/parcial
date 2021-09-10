47334d32aafcf8a43074660ee68d8955a6dc631f
RubidiaPaz <90374939+RubidiaPaz@users.noreply.github.com>
2021-09-10 13:08:59 -0600

    Update parcial.java

--- parcial.java
+++ parcial.java
@@ -6,14 +6,14 @@ class parcial {
 
     
     public static void main(String[] args) {
-        String patron = ("(<li>|<ul>|<a)|([a-zA-Z]+)|([>|<|=]+)|([0-9]+)|([(|)]+)|([{|}]+)|([(.html)]+)|((html)+)|([>a-zA-Z<]+)|(;)");
+        String patron = ("(<li>|<ul>|<a)|(</li>|</ul>|/a>)|(\"[^\"]*\")|(>.*?<)|([a-z,A-Z,0-9]+)|([>|<|=]+)");
         
-        String Texto= "<ul> <li> <a  href = page1 .html> ";
+        String Texto= "<ul> <li> <a href = \"page1.html\" >other page</a> </li> <li> <a href = \"page2.html\">One More</a> </li> <li> <a href = \"page3.html\">About me</a> </li> </ul>";
         
         Pattern p = Pattern.compile(patron);
         Matcher matcher = p.matcher(Texto);
         
-        System.out.println("Guia 2 Compiladores");
+        System.out.println("Ejercicio 2 de Parcial1");
         while (matcher.find())
         {
             String tokenTipo1 = matcher.group(1);
@@ -22,60 +22,37 @@ class parcial {
                 System.out.println("Etiqueta de apertura: "+tokenTipo1);
             }
             
-            String tokenTipo8 = matcher.group(8);
-            if(tokenTipo8 != null)
-            {
-                System.out.println("Extension: "+tokenTipo8);
-            }
-
             String tokenTipo2 = matcher.group(2);
             if(tokenTipo2 != null)
             {
-                System.out.println("Variables: "+tokenTipo2);
-            }
-            
+                System.out.println("Etiqueta de cierre: "+tokenTipo2);
+            } 
+
             String tokenTipo3 = matcher.group(3);
             if(tokenTipo3 != null)
             {
-                System.out.println("Operador: "+tokenTipo3);
+                System.out.println("Referencia: "+tokenTipo3);
             }
-            
+
             String tokenTipo4 = matcher.group(4);
             if(tokenTipo4 != null)
             {
-                System.out.println("Numeros: "+tokenTipo4);
+                System.out.println("Cadena de texto: "+tokenTipo4);
             }
-            
+
             String tokenTipo5 = matcher.group(5);
             if(tokenTipo5 != null)
             {
-                System.out.println("Parentesis: "+tokenTipo5);
+                System.out.println("Variable: "+tokenTipo5);
             }
             
+
             String tokenTipo6 = matcher.group(6);
             if(tokenTipo6 != null)
             {
-                System.out.println("Llaves: "+tokenTipo6);
+                System.out.println("Operador: "+tokenTipo6);
             }
             
-            String tokenTipo7 = matcher.group(7);
-            if(tokenTipo7 != null)
-            {
-                System.out.println("Punto: "+tokenTipo7);
-            }
-            
-            String tokenTipo9 = matcher.group(9);
-            if(tokenTipo9 != null)
-            {
-                System.out.println("Extension: "+tokenTipo9);
-            } 
-
-            String tokenTipo10 = matcher.group(10);
-            if(tokenTipo10 != null)
-            {
-                System.out.println("Punto y coma: "+tokenTipo10);
-            }
-             
         }
         
     }
